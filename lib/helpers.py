f4ad37b9fa247656117d47b4ca43c0483c418875
Martin <martingarcia804@yahoo.com>
2024-01-06 01:21:26 -0600

    Add functionality of remove_song, list_all_songs and add_song

--- lib/helpers.py
+++ lib/helpers.py
@@ -21,7 +21,7 @@ def exit_program():
     exit()
 
 
-#functions for artist class
+#functions for artist vvvvv
 def add_artist():
     print("Adding artist")
 
@@ -47,15 +47,27 @@ def find_artist_by_id(artist_id):
 def remove_artist():
     print("Removing artist...")
 
-    print("🧑‍🎤🎹🎸Artists Available🎸🎹🧑‍🎤")
     list_all_artists()
 
     deleted_artist_id = input ("Please enter the artist's id to remove: ")
 
-    artist_to_delete = find
+    artist_to_delete = find_artist_by_id(deleted_artist_id)
+
+    if artist_to_delete:
+        Artist.all_artists.remove(artist_to_delete)
+        print(f"Done. ❌{artist_to_delete.name}❌ successfully removed.")
+
+        query = "DELETE FROM artists WHERE id = ?"
+        execute_query(query, params=(artist_to_delete.artist_id,))
+
+        print("Remaining artists in the database:")
+        execute_query("SELECT * FROM artists")
+
+    else:
+        print("Hmm.. There doesn't seem to be an artist with that ID.")
 
 def list_all_artists():
-    print(f"Listing all artists:")
+    print(f"🎸🎹🧑‍🎤Available Artists🧑‍🎤🎹🎸")
 
     list_of_artists = None
     artists_found = False
@@ -67,11 +79,7 @@ def list_all_artists():
             artists_found = True
     if not artists_found:
         print("Oh no! There are currently no existing artists.. :(")
-#functions for artist class
-
-
-
-
+#functions for artist ^^^
 
 
 #functions for song class
@@ -80,13 +88,12 @@ def add_song():
 
     song_title = input('Song Title: ')
 
-    print('🎤🎹🎸Available Artists🎤🎹🎸')
     list_all_artists()
 
     songs_artist_id = input('Enter the ID of the artist to assign your song!(`NA` if not assigning): ')
 
     #In case NA is entered
-    artist = find_artist_helper(songs_artist_id) if songs_artist_id.lower() != 'na' else None
+    artist = find_artist_by_id(songs_artist_id) if songs_artist_id.lower() != 'na' else None
 
     #If the artist was found with the provided ID, create a new instance of a Song
     newly_added_song = Song(song_id=song_title.lower().replace(" ", "_"), title=song_title, artist=artist)
@@ -99,40 +106,50 @@ def add_song():
     else:
         print(f"Nicely done! 🎶🎶{newly_added_song}🎶🎶 successfully added!")
 
-
-
 def remove_song():
-    print("Removing song..")
+    print("Removing Song...")
 
-    deleted_song_id = input("Please enter the song's ID ")
+    list_all_songs()
 
-    removed_song = None
+    deleted_song_id = input("Please enter the song's ID to remove: ")
 
-    for song in Song.all_songs:
-        if song.song_id == deleted_song_id:
-            removed_song = song
-            Song.all_songs.remove(song)
-            break
+    removed_song = find_song_by_id(deleted_song_id)
     
     if removed_song:
-        print(f"{removed_song} has now been removed.")
+        Song.all_songs.remove(removed_song)
+        print(f"Done. ❌{removed_song}❌ has now been removed.")
+
+        query = "DELETE FROM songs WHERE song_id = ?"
+        execute_query(query, params=(removed_song.song_id,))
     else:
         print("Uh oh. It seems there is no song by that ID.")
 
 def list_all_songs():
 
-    print("Listing all songs...")
+    print("🎸🎹🧑‍🎤Songs Available🧑‍🎤🎹🎸")
 
-    list_of_songs = None
-    songs_found = False
+    song_list = None
+    found_songs = False
 
     for song in Song.all_songs:
         if song:
-            list_of_songs = song
-            print(f'🎶🎹🎸{song.title}🎸🎹🎶')
-            songs_found = True
-    if not songs_found:
+            song_list = song
+            print(f"🎶{song.title} ID:{song.song_id}🎶")
+            found_songs = True
+    if not found_songs:
         print("Oh no! There are currently no existing songs.. :(")
+
+def find_song_by_id(song_id):
+    for song in Song.all_songs:
+        if song.song_id == song_id:
+            return song
+    return None
+
+
+
+
+
+
 
 def add_song_to_favorites():
     print("Adding song to your favorites!...")
@@ -146,8 +163,6 @@ def add_song_to_favorites():
     #     print(f"Success! Song now favorited: {song_to_be_favorited.title}")
     # else:
     #     print(f"Hmmm.. It looks like there is no song with the ID of {needed_song_id}")
-
-
 
 def remove_favorited_song():
     print("Removing Favorited song...")
@@ -168,6 +183,7 @@ def remove_favorited_song():
         print(f"Hmm... It seems there is no song with the ID of {removed_favorited_song_id} in your Favorites.")
 
 def list_favorited_songs():
+    
     print("Listing all of your favorited songs!..")
 
     favorited_song_list = None
@@ -180,3 +196,4 @@ def list_favorited_songs():
             favorite_songs_found = True
         if not favorite_songs_found:
             print("Hmm... There doesn't seem to be any songs in your Favorites at the moment. Sorry!")
+#functions for song class
